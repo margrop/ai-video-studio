4f59c24a9b0de202ea8aa8b311c7c6c79b344df4
Margrop <dennis_1125@163.com>
2026-08-03 19:49:37 +0800

    feat: add Volcengine Ark Agent Plan video provider

---
+++ providers/volcengine/__init__.py
@@ -0,0 +1,7 @@
+"""Volcengine Ark Agent Plan video adapters."""
+
+from .video import VolcengineAgentPlanVideoProvider
+
+VolcengineVideoProvider = VolcengineAgentPlanVideoProvider
+
+__all__ = ["VolcengineAgentPlanVideoProvider", "VolcengineVideoProvider"]
